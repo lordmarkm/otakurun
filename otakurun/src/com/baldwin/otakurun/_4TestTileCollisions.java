f838f17d01a3fc553ca992dec9588d67859ba0bf
mbmartinez <mbmartinez@mbmartinez-Inspiron-N5010>
2013-02-15 18:35:33 +0800

    further fixes for bin problem

--- otakurun/src/com/baldwin/otakurun/_4TestTileCollisions.java
+++ otakurun/src/com/baldwin/otakurun/_4TestTileCollisions.java
@@ -2,7 +2,6 @@ package com.baldwin.otakurun;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.baldwin.libgdx.commons.BasePlatform;
